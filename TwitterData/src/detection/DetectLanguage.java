b4ea6ca3aaf488f6dbe27bb06dc392e002331f20
hitarth <t.hitarth@gmail.com>
2014-04-02 00:43:12 -0400

    minor changes

---
+++ TwitterData/src/detection/DetectLanguage.java
@@ -0,0 +1,24 @@
+package detection;
+
+import java.util.ArrayList;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+import com.cybozu.labs.langdetect.Language;
+
+public class DetectLanguage {
+    public void init(String profileDirectory) throws LangDetectException {
+        DetectorFactory.loadProfile(profileDirectory);
+    }
+    public String detect(String text) throws LangDetectException {
+        Detector detector = DetectorFactory.create();
+        detector.append(text);
+        return detector.detect();
+    }
+    public ArrayList<Language> detectLangs(String text) throws LangDetectException {
+        Detector detector = DetectorFactory.create();
+        detector.append(text);
+        return detector.getProbabilities();
+    }
+}
--- TwitterData/src/twitterproject/TweetsFromJackson.java
+++ TwitterData/src/twitterproject/TweetsFromJackson.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,28 +14,51 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.cybozu.labs.langdetect.LangDetectException;
+
 import twitterPojo.TwitterPojo;
+import detection.DetectLanguage;
 
 public class TweetsFromJackson {
+	
+	private static String profilePath;
 
   public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
-   
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+       extends Mapper<Object, Text, Text, Text>{
+	  
+    DetectLanguage language;
+	String profilePath = TweetsFromJackson.profilePath;
+	protected void setup(Context context)
+            throws IOException,
+                   InterruptedException{
+		try {
+			language = new DetectLanguage();
+			language.init(profilePath);
+		} catch (LangDetectException e) {
+			System.out.println("Invalid profile files path");
+			e.printStackTrace();
+		}
+	}
      
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
      String line = value.toString();
-
-    if(line.startsWith("{\"createdAt\":")){
-                TwitterPojo tweet = new ObjectMapper().readValue(line.getBytes(),TwitterPojo.class);
-                StringBuilder data = new StringBuilder();
-                data.append(tweet.getText()).append("¿").append(tweet.getCreatedAt());
-                word.set(data.toString());
-                System.out.println(data.toString());
-                context.write(word, one);
-            }
+     TwitterPojo tweet = new ObjectMapper().readValue(line.getBytes(),TwitterPojo.class);
+     // get the detected language
+     try {
+    	 	//System.out.println(tweet.getText());
+		if("en".equals(language.detect(tweet.getText()))){
+		     // emit the the given data
+		     context.write(value 	,new Text(""));
+		 }
+		else {
+			 //context.write(new Text(tweet.getText()+" non english detected"),new IntWritable(1));
+		}
+	} catch (LangDetectException e) {
+		// TODO Auto-generated catch block
+		//e.printStackTrace();
+	}
+     
     }
   }
  
@@ -42,17 +66,19 @@ public class TweetsFromJackson {
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length != 2) {
-      System.err.println("Usage: Twitter <in> <out>");
+    if (otherArgs.length != 3) {
+      System.err.println("Usage: Twitter <in> <out> <profile_path>");
       System.exit(2);
     }
+    TweetsFromJackson.profilePath = otherArgs[2];
     Job job = new Job(conf, "Twitter data");
     job.setJarByClass(TweetsFromJackson.class);
     job.setMapperClass(TokenizerMapper.class);
     //job.setCombinerClass(IntSumReducer.class);
     //job.setReducerClass(IntSumReducer.class)
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+   // job.setMapOutputValueClass(NullWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
     FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
